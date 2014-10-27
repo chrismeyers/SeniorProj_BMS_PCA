346e4af8c95b76d0b8d5534ec1b23cd8a0989d5f
chrismeyers <cm.02.93@gmail.com>
2014-10-27 16:13:03 -0400

    modularized code.  scores plot now plots on new worksheet.

--- PCA_Addin/Classes/PCA_graphing.cs
+++ PCA_Addin/Classes/PCA_graphing.cs
@@ -33,7 +33,8 @@ namespace PCA_Addin
         /// <param name="col1">The first column to be used in plot (PC1)</param>
         /// <param name="col2">The seconf column to be used in plot (PC2)</param>
         /// <author>Rowan Senior Project - Chris Meyers</author>
-        public void scoresPlot(string col1, string col2){
+        public void scoresPlot(string col1, string col2)
+        {
             //MessageBox.Show("Column " + col1 + " and " + "column " + col2 + " selected.");
             String pc1 = col1.ToUpper();
             String pc2 = col2.ToUpper();
@@ -48,105 +49,131 @@ namespace PCA_Addin
             activeWorkbook = Globals.ThisAddIn.Application.ActiveWorkbook;
 
             //Define chart variables
-            Excel.ChartObjects xlCharts = (Excel.ChartObjects)ws.ChartObjects(Type.Missing);
-            Excel.ChartObject myChart = (Excel.ChartObject)xlCharts.Add(10, 80, 300, 250);
+            Excel.Worksheet wsChart = (Excel.Worksheet)activeWorkbook.Worksheets.Add();
+            Excel.ChartObjects xlCharts = (Excel.ChartObjects)wsChart.ChartObjects(Type.Missing);
+            Excel.ChartObject myChart = (Excel.ChartObject)xlCharts.Add(0, 0, 600, 300);
             Excel.Chart chartPage = myChart.Chart;
 
+            wsChart.Name = "Scores Plot";
             chartPage.ChartType = Excel.XlChartType.xlXYScatter;
             myChart.Chart.HasTitle = true;
             String currentChartName = ws.Cells[1, columnLetterToNumber(pc1)].Value.ToString() + " vs. " +
                 ws.Cells[1, columnLetterToNumber(pc2)].Value.ToString();
             chartPage.ChartTitle.Text = currentChartName;
 
-            //Makes chart on new chartsheet instead of current worksheet.
-            //chartPage.Location(Excel.XlChartLocation.xlLocationAsNewSheet, "Scores Plot"); 
-
             //Define data strucures needed for data manipulation and plotting.
             int numRows = ws.UsedRange.Rows.Count;
             ArrayList groups = new ArrayList();
             ArrayList groupPoints = new ArrayList();
             Dictionary<String, ArrayList> plotData = new Dictionary<String, ArrayList>();
-             
+
             if (errorCheck(pc1, pc2)){
                 //Asks for new valid columns upon error.
                 newColumns();
             }
             else{
-                //Inititally make previous group first group name.
-                //This will be used to check if the group changed.
-                //If it did, add the previous data collected to a dictionary
-                //with Key=previous group and Value=Arraylist of the data 
-                //corresponding to the key.
-                String prevGrp = ws.Cells[2, 2].Value.ToString();
+                //Process selected columns and store in a dictionary.
+                plotData = createDictionary(pc1, pc2, ws, numRows, groups, groupPoints);
 
-                for (int i = 2; i < numRows+1; i++) {
-                    String currentGrp = ws.Cells[i, 2].Value.ToString();
-
-                    //Add the data for the previous group to the dictionary
-                    if (!currentGrp.Equals(prevGrp)){
-                        groups.Add(prevGrp);
-                        //ArrayList needs to be cloned or data from next group will overwrite
-                        plotData.Add(prevGrp, (ArrayList)groupPoints.Clone());
-                        groupPoints.Clear();
-                    }
-                    
-
-                    //Get current row x and y values.
-                    //Add them to an array, add this array to the group ArrayList
-                    Double[] currentPoints = new Double[2];
-                    String currentX = ws.Cells[i, columnLetterToNumber(pc1)].Value.ToString();
-                    String currentY = ws.Cells[i, columnLetterToNumber(pc2)].Value.ToString();
-                    //MessageBox.Show(currentGrp + ": " + currentX + ", " + currentY);
-                    currentPoints[0] = Convert.ToDouble(currentX);
-                    currentPoints[1] = Convert.ToDouble(currentY);
-                    //MessageBox.Show(currentGrp + ": " + currentPoints[0] + ", " + currentPoints[1]);
-                    groupPoints.Add(currentPoints);
-
-                    //Add the data for the last row
-                    if (i == numRows){
-                        groups.Add(currentGrp);
-                        plotData.Add(currentGrp, (ArrayList)groupPoints.Clone());
-                        groupPoints.Clear();
-                    }
-
-                    prevGrp = currentGrp;
-                }
-
-                /*
-                chartRange = ws.get_Range("B:B" + "," + pc1 + ":" + pc1 + "," + pc2 + ":" + pc2);
-                chartPage.SetSourceData(chartRange, misValue);
-                 */
-                 
                 //Loop through each group and make a series for each one.
-                foreach (String group in groups) {
-                    //MessageBox.Show(group);
-                    Excel.SeriesCollection seriesCollection = (Excel.SeriesCollection)myChart.Chart.SeriesCollection();
-                    Excel.Series currentGroup = seriesCollection.NewSeries();
-                    currentGroup.Name = group;
-
-                    //Get point values from associated group.
-                    ArrayList values = new ArrayList();
-                    values = plotData[group];
-
-                    Double[] x = new Double[values.Count];
-                    Double[] y = new Double[values.Count];
-
-                    //Loop through all points in group and save to x and y array.
-                    for (int val = 0; val < values.Count; val++) {
-                        Double[] points = (Double[])values[val];
-                        //MessageBox.Show(points[0] + ", " + points[1]);
-
-                        x[val] = points[0];
-                        y[val] = points[1];
-                        
-                    }
-                    currentGroup.XValues = x; //X values (PC1)
-                    currentGroup.Values = y;  //Y values (PC2)
-
-                }
-                
+                Excel.SeriesCollection seriesCollection = (Excel.SeriesCollection)myChart.Chart.SeriesCollection();
+                generateScoresPlot(groups, seriesCollection, plotData);
             }
             //MessageBox.Show("Value is: " + (ws.Cells[10,10] as Excel.Range).Value);
+        }
+
+        /// <summary>
+        /// Creates a dictionary with groups as Keys and selected principal component points as Values.
+        /// </summary>
+        /// <param name="pc1">the first principal component selected</param>
+        /// <param name="pc2">the second principal component selected</param>
+        /// <param name="ws">the current worksheet</param>
+        /// <param name="numRows">number of rows in use in the current worksheet</param>
+        /// <param name="groups">ArrayList that holds data for all the groups</param>
+        /// <param name="groupPoints">ArrayList that holds the data points for the current group</param>
+        /// <author>Rowan Senior Project - Chris Meyers</author>
+        private Dictionary<String, ArrayList> createDictionary(String pc1, String pc2, Excel.Worksheet ws, 
+                                                                int numRows, ArrayList groups, ArrayList groupPoints) { 
+            //Inititally make previous group first group name.
+            //This will be used to check if the group changed.
+            //If it did, add the previous data collected to a dictionary
+            //with Key=previous group and Value=Arraylist of the data 
+            //corresponding to the key.
+            Dictionary<String, ArrayList> plotData = new Dictionary<String,ArrayList>();
+            String prevGrp = ws.Cells[2, 2].Value.ToString();
+
+            for (int i = 2; i < numRows + 1; i++)
+            {
+                String currentGrp = ws.Cells[i, 2].Value.ToString();
+
+                //Add the data for the previous group to the dictionary
+                if (!currentGrp.Equals(prevGrp))
+                {
+                    groups.Add(prevGrp);
+                    //ArrayList needs to be cloned or data from next group will overwrite
+                    plotData.Add(prevGrp, (ArrayList)groupPoints.Clone());
+                    groupPoints.Clear();
+                }
+
+
+                //Get current row x and y values.
+                //Add them to an array, add this array to the group ArrayList
+                Double[] currentPoints = new Double[2];
+                String currentX = ws.Cells[i, columnLetterToNumber(pc1)].Value.ToString();
+                String currentY = ws.Cells[i, columnLetterToNumber(pc2)].Value.ToString();
+                //MessageBox.Show(currentGrp + ": " + currentX + ", " + currentY);
+                currentPoints[0] = Convert.ToDouble(currentX);
+                currentPoints[1] = Convert.ToDouble(currentY);
+                //MessageBox.Show(currentGrp + ": " + currentPoints[0] + ", " + currentPoints[1]);
+                groupPoints.Add(currentPoints);
+
+                //Add the data for the last row
+                if (i == numRows)
+                {
+                    groups.Add(currentGrp);
+                    plotData.Add(currentGrp, (ArrayList)groupPoints.Clone());
+                    groupPoints.Clear();
+                }
+
+                prevGrp = currentGrp;
+            }
+            return plotData;
+        }
+
+        /// <summary>
+        /// Generates a scores plot from the selected Principal Components.
+        /// </summary>
+        /// <param name="groups">ArrayList that holds data for all the groups</param>
+        /// <param name="seriesCollection">a collection object that holds all the series</param>
+        /// <param name="plotData">the dictionary that holds data required to plot(Keys:groups, Values:PC points)</param>
+        /// <author>Rowan Senior Project - Chris Meyers</author>
+        private void generateScoresPlot(ArrayList groups, Excel.SeriesCollection seriesCollection, Dictionary<String, ArrayList> plotData) {
+            foreach (String group in groups){
+                //MessageBox.Show(group);
+
+                Excel.Series currentGroup = seriesCollection.NewSeries();
+                currentGroup.Name = group;
+
+                //Get point values from associated group.
+                ArrayList values = new ArrayList();
+                values = plotData[group];
+
+                Double[] x = new Double[values.Count];
+                Double[] y = new Double[values.Count];
+
+                //Loop through all points in group and save to x and y array.
+                for (int val = 0; val < values.Count; val++)
+                {
+                    Double[] points = (Double[])values[val];
+                    //MessageBox.Show(points[0] + ", " + points[1]);
+
+                    x[val] = points[0];
+                    y[val] = points[1];
+
+                }
+                currentGroup.XValues = x; //X values (PC1)
+                currentGroup.Values = y;  //Y values (PC2)
+            }
         }
 
         /// <summary>
@@ -162,6 +189,7 @@ namespace PCA_Addin
         /// Converts column letter to its associated numerical value.
         /// Ex: A->1, B->2, etc.
         /// </summary>
+        /// <param name="col">string representation of the column to be converted</param>
         /// <author>Rowan Senior Project</author>
         private int columnLetterToNumber(String col) {
             //This conversion algorithm was taken from:
@@ -183,6 +211,8 @@ namespace PCA_Addin
         /// <summary>
         /// Checks for column input errors.
         /// </summary>
+        /// <param name="pc1">the first principal component selected</param>
+        /// <param name="pc2">the second principal component selected</param>
         /// <author>Rowan Senior Project - Chris Meyers, Christian Marin</author>
         private Boolean errorCheck(String pc1, String pc2) {
             Boolean error = false;
